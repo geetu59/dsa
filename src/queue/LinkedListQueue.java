9cab5de9458e85ecdac7d249120d067971edf56f
geetu59 <geetanjaligomber98@gmail.com>
2024-03-02 23:59:47 +0530

    Queue: Basic operations using inbuilt lib

--- src/queue/LinkedListQueue.java
+++ src/queue/LinkedListQueue.java
@@ -2,6 +2,9 @@ package queue;
 
 import linkedlist.singlell.Node;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /*Implement queue using linkedlist. O(1) for all operations.
  * We will use single linked list, where we maintain both head and tail. head-> front and tail is rear.
  * Enqueue can be done by doing rear.next and deque can be done by updating the front as front.next.
@@ -28,6 +31,24 @@ public class LinkedListQueue {
         System.out.println(queue.getFront());
         System.out.println(queue.getRear());
         System.out.println(queue.getSize());
+
+        //Using built-in classes
+        //Can use LinkedList class or ArrayDeque class. Difference is one is implemented using ll and other using array
+        //ll has worst case time complexity as O(1) and array has amortised(avg) as O(1) becaz it might resize if array is full
+        //Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue1 = new ArrayDeque<>();
+
+        //both of them adds element but offer returns false if queue is full and add throws exception
+        System.out.println(queue1.offer(1));
+        System.out.println(queue1.add(2));
+
+        //returns top element i.e. head i.e. front but element throws exception and peek returns null if queue is empty
+        System.out.println(queue1.peek());
+        System.out.println(queue1.element());
+
+        //returns front and remove element. Null if queue is empty by poll and exception by remove
+        System.out.println(queue1.poll());
+        System.out.println(queue1.remove());
     }
 
     private int getSize() {
